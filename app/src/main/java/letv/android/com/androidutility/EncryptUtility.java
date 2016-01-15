b53f8cef833d2898ea802afe690ae6f6dba3c600
mayongsheng <mayongsheng@letv.com>
2016-01-15 14:07:01 +0800

    add new utility

---
+++ app/src/main/java/letv/android/com/androidutility/EncryptUtility.java
@@ -0,0 +1,32 @@
+package letv.android.com.androidutility;
+
+import java.security.MessageDigest;
+
+/**
+ * Created by mayongsheng on 16/1/10.
+ */
+public class EncryptUtility {
+
+    public final static String MD5(String s) {
+        char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                'a', 'b', 'c', 'd', 'e', 'f' };
+        try {
+            byte[] btInput = s.getBytes();
+            MessageDigest mdInst = MessageDigest.getInstance("MD5");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (int i = 0; i < j; i++) {
+                byte byte0 = md[i];
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+}
